5e9897fefc08d5d0f1d9cc36b7f1e1c44c13bc49
elene shetsiruli <ellebuto@gmail.com>
2024-03-22 14:38:11 +0400

    "add openHours"

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  return (
+    <footer className="footer">
+      <div className="order">
+        {isOpen ? (
+          <p>We're open until {closeHour}:00 o'clock</p>
+        ) : (
+          <p>We're close now until {openHour}:00 o'clock</p>
+        )}
+        <button className="btn">Order</button>
+      </div>
+    </footer>
+  );
 };
--- src/data.tsx
+++ src/data.tsx
@@ -1,1 +1,61 @@
+import FocacciaImage from "./pizzas/focaccia.jpg";
+import MargheritaImage from "./pizzas/margherita.jpg";
+import SpinaciImage from "./pizzas/spinaci.jpg";
+import FunghiImage from "./pizzas/funghi.jpg";
+import SalaminoImage from "./pizzas/salamino.jpg";
+import ProsciuttoImage from "./pizzas/prosciutto.jpg";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: FocacciaImage,
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: MargheritaImage,
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: SpinaciImage,
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: FunghiImage,
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: SalaminoImage,
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: ProsciuttoImage,
+    soldOut: false,
+  },
+];
+
+interface pizzaData {
+  name: string;
+  ingredients: string;
+  price: number;
+  photoName: string;
+  soldOut: boolean;
+}
+
+export default pizzaData;
